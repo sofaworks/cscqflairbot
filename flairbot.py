c40247c0a777a17c32cadb9fe2b91ab56abe2ce6
Sofa Assassin <sofaassassin@users.noreply.github.com>
2018-02-06 13:56:23 -0500

    Improve flair messages and uses full listings
    Previous version only pulled from API listing once, which only returns at most 100 items.
    We now calculate karma based on 1000 'newest' comments, or 1000 'top', and use whichever is
    greater. Same for submission karma.

--- flairbot.py
+++ flairbot.py
@@ -5,7 +5,6 @@
 import os
 import logging
 import itertools
-
 from collections import namedtuple
 
 import praw
@@ -16,7 +15,6 @@ FlairMapping = namedtuple('FlairMapping', ['karma', 'flair_class'])
 
 FLAIR_MAPPINGS = sorted([FlairMapping(k, 'over-{}-karma'.format(k))
     for k in (500, 1000, 3000, 5000, 10000, 20000)], key=lambda fm: fm.karma, reverse=True)
-
 
 def get_environment_configuration():
     '''Get configuration values specified in environment variables'''
@@ -32,7 +30,6 @@ def get_environment_configuration():
 
 
 class FlairBot:
-    # the flairs we care about
     def __init__(self, configuration):
         self.reddit = praw.Reddit(user_agent=configuration['USER_AGENT'],
                 client_id=configuration['CLIENT_ID'],
@@ -44,37 +41,41 @@ class FlairBot:
         self.subreddit = configuration['SUBREDDIT']
 
         self.send_confirmations = True
-        #self.use_comment_karma = True
-        #self.use_submission_karma = True
+        self.use_comment_karma = True
+        self.use_submission_karma = True
+
 
     def check_pms(self):
-        '''Check bot's unread PMs - we only care about PMs with the title "Flair Me"'''
+        '''Check bot's unread PMs - we only care about PMs with the
+        title "Flair Me" or "Change Flair Text"'''
         flair_requests = dict()
         flair_text_changes = dict()
         ignored_messages = []
         for msg in self.reddit.inbox.unread(limit=None):
             author = msg.author.name
             if (msg.subject.strip().lower() == 'flair me') and (author not in flair_requests):
-                logging.info("Calculate flair request for: {}".format(author))
+                logging.info("request=CalculateFlair user={}".format(author))
                 flair_requests[author] = msg
             elif (msg.subject.strip().lower() == 'change flair text') and (author not in flair_text_changes):
-                logging.info("Change flair text request for: {}".format(author))
+                logging.info("request=ChangeFlairText user={}".format(author))
                 flair_text_changes[author] = msg
             else:
                 ignored_messages.append(msg)
         self.reddit.inbox.mark_read(ignored_messages)
 
-        # now process flair_requests
-        self.process_flair_requests(flair_requests)
-
-        # process flair text requests
+        # process flair text requests first, as they are
+        # much faster to process
         self.process_flair_text_requests(flair_text_changes)
+
+        # now process flair_requests, as these can take a long time
+        # since it has to generate up to 40 API calls per user
+        self.process_flair_requests(flair_requests)
 
     def change_flair_text(self, subreddit, redditor, new_flair_text):
         current_flair = next(subreddit.flair(redditor=redditor))
         flair_class = current_flair['flair_css_class']
         try:
-            logging.info("Changing flair text for {} to '{}'".format(redditor, new_flair_text))
+            logging.info("action=ChangeFlairText user={} text={}".format(redditor, new_flair_text))
             subreddit.flair.set(redditor, new_flair_text, flair_class)
         except Exception as e:
             logging.exception("Problem occurred setting flair text for {}".format(redditor))
@@ -89,38 +90,68 @@ class FlairBot:
             flair_text = msg.body.splitlines()[0].strip()
             success = self.change_flair_text(sub, author, flair_text)
             if success:
-                reply = "Flair text changed to: **{}**".format(flair_text)
+                reply = "Flair text changed to: **{}**. Your flair color has not changed.".format(flair_text)
             else:
                 reply = "There was a problem changing your flair text. Try again. If this keeps occurring, please contact the {} mods".format(self.subreddit)
             msg.reply(reply)
             msg.mark_read()
 
 
-    def calculate_subreddit_karma(self, redditor):
+    def calculate_subreddit_karma(self, redditor, listing_type='top'):
         total_karma = 0
-        for thing in itertools.chain(redditor.comments.top('all'), redditor.submissions.top('all')):
+        if listing_type not in ('new', 'top'):
+            logging.warn("Unsupported listing type {} passed for calculating subreddit karma".format(listing_type))
+            raise TypeError("Unsupported listing type {}".format(listing_type))
+
+        comment_method = getattr(redditor.comments, listing_type)
+        submission_method = getattr(redditor.submissions, listing_type)
+        for thing in itertools.chain(comment_method(limit=None), submission_method(limit=None)):
             if thing.subreddit.display_name == self.subreddit:
                 total_karma += thing.score
         return total_karma
 
 
+    def generate_flair_message(self, **kwargs):
+        base_flair_message = ('Karma by **top posts**: **{top_karma}**  \n'
+          'Karma by **new posts**: **{new_karma}**  \n'
+          'Your highest karma was calculated using **{karma_type} posts**.  \n\n'
+          '{msg}')
+
+        return base_flair_message.format(**kwargs)
+
+
     def process_flair_requests(self, flair_requests):
         '''Given a map of users and their messages, check for their
         flair level'''
+
         sub = self.reddit.subreddit(self.subreddit)
         for author, msg in flair_requests.items():
-            karma = self.calculate_subreddit_karma(msg.author)
+            top_karma = self.calculate_subreddit_karma(msg.author, 'top')
+            new_karma = self.calculate_subreddit_karma(msg.author, 'new')
+
             # now use this to determine what tier it is
+            if top_karma > new_karma:
+                karma = top_karma
+                karma_type = 'top'
+            else:
+                karma = new_karma
+                karma_type = 'new'
+
             flair_type = None
             for flair in FLAIR_MAPPINGS:
                 if karma >= flair.karma:
                     flair_type = flair
                     break
 
+            fmt_dict = {
+                'karma_type': karma_type,
+                'top_karma': top_karma,
+                'new_karma': new_karma
+            }
+
             if not flair_type:
                 # send a PM and say nothing will change
-                print("No flair lol")
-                msg.reply('Calculated Karma: **{}**. Too low for flair.'.format(karma))
+                msg.reply(self.generate_flair_message(**fmt_dict, msg='Your calculated karma was too low for flair'))
             else:
                 # get the current flair for the user
                 current_flair = next(sub.flair(redditor=author))
@@ -131,12 +162,14 @@ class FlairBot:
                 if flair_class:
                     # if flair_class is None but flair_type isn't, just set flair_type
                     if flair_class == flair_type.flair_class:
-                        msg.reply('Calculated Karma: **{}**. Flair class is same as what you have now, so no changes.'.format(karma))
+                        msg.reply(self.generate_flair_message(**fmt_dict,
+                                      msg='Flair class is same as what you have now, so no changes.'))
+
                     else: # determine if the new flair class is higher than the current one
                         current_flair_karma_class = int(flair_class.split('-')[1])
                         if current_flair_karma_class > flair_type.karma:
                             # our new flair is worse than the old one, so don't change it
-                            msg.reply('Calculated Karma: **{}**. New flair would be worse than your current, so no changes.'.format(karma))
+                            msg.reply(self.generate_flair_message(**fmt_dict, msg='New flair would be worse than current flair, so no changes.'))
                         else:
                             set_new_flair = True
                 else:
@@ -144,11 +177,10 @@ class FlairBot:
 
             if set_new_flair:
                 # change flair class for user
-                logging.info("Setting flair class for {} to '{}'".format(author, flair_type.flair_class))
+                logging.info("action=ChangeFlairClass user={} class={}".format(author, flair_type.flair_class))
                 sub.flair.set(author, css_class=flair_type.flair_class, text=flair_text)
-                msg.reply('Calculated Karma: **{}**. New flair set for karma level {}+'.format(karma, flair_type.karma))
+                msg.reply(self.generate_flair_message(**fmt_dict, msg='Setting new flair for karma level {}+'))
             msg.mark_read()
-
 
 
 if __name__ == '__main__':
